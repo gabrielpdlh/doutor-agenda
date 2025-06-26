5b986d20b75ec1effa3e2bf127a7496629733eda
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-06-26 19:05:48 -0300

    feat: add functionality to create clinic

--- src/app/clinic-form/components/form.tsx
+++ src/app/clinic-form/components/form.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
+import { createClinic } from "@/actions/create-clinic";
 import { Button } from "@/components/ui/button";
 import { DialogFooter } from "@/components/ui/dialog";
 import {
@@ -24,18 +29,25 @@ const ClinicForm = () => {
   const form = useForm<z.infer<typeof clinicFormSchema>>({
     resolver: zodResolver(clinicFormSchema),
     defaultValues: {
-      name:""
-    }
+      name: "",
+    },
   });
 
-  const onSubmit = (data: z.infer<typeof clinicFormSchema>) => {
-    console.log(data);
+  const onSubmit = async (data: z.infer<typeof clinicFormSchema>) => {
+    try {
+      await createClinic(data.name);
+      toast.success("Clínica criada com sucesso!");
+    } catch (error) {
+      console.error(error);
+      toast.error("Erro ao criar clinica!");
+    }
+    redirect("/dashboard");
   };
 
   return (
     <>
       <Form {...form}>
-        <form  className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+        <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
           <FormField
             control={form.control}
             name="name"
@@ -51,7 +63,12 @@ const ClinicForm = () => {
           />
 
           <DialogFooter>
-            <Button type="submit">Criar Clínica</Button>
+            <Button disabled={form.formState.isSubmitting} type="submit">
+              {form.formState.isSubmitting && (
+                <Loader2 className="h-4 w-4 animate-spin" />
+              )}
+              Criar Clínica
+            </Button>
           </DialogFooter>
         </form>
       </Form>
